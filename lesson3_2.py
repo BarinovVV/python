feac91812bf8d6ffa0a1b9545fee5e1089dbbc11
skyvalery <ksk05@bk.ru>
2020-04-22 22:32:13 +0300

    home work 2 for lesson 3 done

---
+++ lesson3_2.py
@@ -0,0 +1,6 @@
+def print_user_data(name='', surname='', year=None, town='', mail='', phone=''):
+    print(f"{name} {surname}, год рождения - {year}, город проживания - {town}, e-mail {mail} телефон {phone}")
+
+
+print_user_data("Валерий", "Баринов", 1970, "Кострома", "ksk05@bk.ru", "+7 910 661 81 00")
+
