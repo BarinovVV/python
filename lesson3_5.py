93d3a9586277ce3896edccd4c833c5d9bac63da5
skyvalery <ksk05@bk.ru>
2020-04-23 14:47:26 +0300

    home work 5 for lesson 3 done

--- lesson3_5.py
+++ lesson3_5.py
@@ -1,5 +1,31 @@
-def sum_numbers(*args):
-    return sum(args)
+def sum_numbers(num_string):
+    num_list = num_string.split()
+    result = 0
+    for i in range(len(num_list)):
+        if num_list[i] != "q":
+            result += float(num_list[i])
+        else:
+            return result
+    return result
 
 
-print(sum_numbers(1, 2, 3))
+user_result = 0
+
+while True:
+    user_numbers = input("Введите числа через пробел ")
+    if 'q' in user_numbers:
+        user_result += sum_numbers(user_numbers)
+        break
+    else:
+        user_result += sum_numbers(user_numbers)
+        print(f"Сумма введенных числе равна {user_result}")
+        print("Продолжайте ввод чисел через пробел для остановки введите в любом месте q")
+
+print(f"Итоговая сумма введенных числе равна {user_result}")
+
+
+
+
+
+
+
