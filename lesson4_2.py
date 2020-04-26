0796d75dcdf2f3b152eb7169622068e575fcd31a
skyvalery <ksk05@bk.ru>
2020-04-26 20:40:34 +0300

    home work 2 for lesson 4  done

--- lesson4_2.py
+++ lesson4_2.py
@@ -1,12 +1,6 @@
 start_list = [300, 4, 56, 77, 4, 99, 345, 84, 78]
-# finish_list =[]
 
-# for i in range(1, len(start_list)):
-#     if start_list[i] > start_list[i-1]:
-#         finish_list.append(start_list[i])
-
-finish_list = [el for i, el in enumerate(start_list) if start_list[i] > start_list[i - 1] and i != 0]
+finish_list = [el for i, el in enumerate(start_list) if i != 0 and start_list[i] > start_list[i - 1]]
 
 print(f"Исходный список {start_list} ")
 print(f"Обработанный список {finish_list} ")
-
