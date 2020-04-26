8d9346b7ca181867323e15abc7ef3d6f7d3e7433
skyvalery <ksk05@bk.ru>
2020-04-26 23:15:28 +0300

    home work 6 for lesson 4 done

--- lesson4_6.py
+++ lesson4_6.py
@@ -1,1 +1,15 @@
-print("start home work 6 for lesson 4")
+from itertools import count, cycle, islice
+
+for i in islice(count(int(input("Введите стартовое число ")), 2), 100):
+    print(i)
+
+print([el for el in islice(count(int(input("Введите стартовое число ")), 5), 100)])
+
+lang = ["python", "java", "perl", "javascript", "php", "cobol", "ada"]
+count = 1
+for item in cycle(lang):
+    if count > len(lang) * 3:
+        break
+    print(item)
+    count += 1
+
