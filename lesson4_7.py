13be88e8a616e4f88367d300184b6ff665082ae3
skyvalery <ksk05@bk.ru>
2020-04-27 10:55:12 +0300

    home work 7 for lesson 4 done

--- lesson4_7.py
+++ lesson4_7.py
@@ -1,1 +1,21 @@
-print("start home work 7 for lesson 4")
+from itertools import count
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return factorial(n - 1) * n
+
+
+def factorial_gen():
+    for f in count(1):
+        yield factorial(f)
+
+
+i = 1
+for el in factorial_gen():
+    print(f"Факториал {i} - {el}")
+    if i == 15:
+        break
+    i += 1
+
