3caa4f538f6fdabc6810a4beb34a7a196aa9ade4
skyvalery <ksk05@bk.ru>
2020-05-04 13:02:13 +0300

    hw 5 for lesson 6 done

--- lesson6_5.py
+++ lesson6_5.py
@@ -1,1 +1,26 @@
-print('start hw 5 for lesson 6')
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(f'Запуск отрисовки {self.title}')
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f'Отрисовка дочернего класса Pen {self.title}')
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f'Отрисовка дочернего класса Pencil {self.title}')
+
+
+class Handle(Stationery):
+    def draw(self):
+        print(f'Отрисовка дочернего класса Handle {self.title}')
+
+
+Pen('Шариковя ручка').draw()
+Pencil('Чертежный карандаш').draw()
+Handle('Несмываемый маркер').draw()
